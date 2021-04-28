aabbfd3da74ca89ee0d2edc8fbfb83827868b268
Cole Flournoy <flournoy.cole@gmail.com>
2021-04-28 18:53:29 -0400

    commit tree dd329dc5e49f3c3f5d0e11f5bb87c6f03e995c7f

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+class Polygon {
+  constructor(sides){
+    this.sides = sides
+  }
+  get countSides(){
+    return this.sides.length
+  }
+  get perimeter(){
+    return this.sides.reduce((accumulator, currentValue) => accumulator += currentValue,  0)
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid(){
+    let tri = this.sides
+    if (tri[0] + tri[1] <= tri[2] || tri[1] + tri[2] <= tri[0] || tri[2] + tri[0] <= tri[1]){
+      return false
+    } else {
+      return true
+    }
+  }
+}
+
+class Square extends Polygon {
+  get isValid(){
+    let sq = this.sides
+    if (sq[0] === sq[1] && sq[1] === sq[2] && sq[2] === sq[3]){
+      return true
+    } else {
+      return false
+    }
+  }
+
+  get area(){
+    return this.sides[0] * this.sides[0]
+  }
+}
